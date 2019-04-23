756ca8a8f06517a5501b619e16e65b829acabb48
Mac <mac@Gerard.local>
2019-04-23 12:17:45 +0200

    first commit

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,5 @@ const router  = express.Router();
 router.get('/', (req, res, next) => {
   res.render('index');
 });
-//--------Creat a user ___________
-router.get("/form_car",(req,res,next)=>{
-  res.render("form_car.hbs")
-});
-router.post("/dashboard/car/",(req,res,next)=>{
-  const {firstName, description}= req.body 
-  Car.create(
-    { firstName, description }
-  )
-  .then (carDoc =>{
-    res.redirect("/names")
-    console.log(carDoc)
-  })
-  .catch(err =>next(err))
-});
-//---------
+
 module.exports = router;
