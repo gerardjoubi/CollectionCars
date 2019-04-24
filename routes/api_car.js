47352bca76c5c2c1a28aba54259bb1a84c07b231
Mac <mac@Gerard.local>
2019-04-24 16:21:10 +0200

    affichage des forms ok

--- routes/api_car.js
+++ routes/api_car.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const carModel = require("../models/car");
 const userModel = require("../models/users");
+const pieceModel = require("../models/piece");
 // ------------------------------------------------------
 // ------------------creat car-------------------
 // router.get("/form_car",(req,res,next)=>{
@@ -26,14 +27,17 @@ router.get("/user_page/:id", (req, res, next) => {
     .findById(req.params.id)
     .then(userDoc => {
       console.log(userDoc);
-      carModel.find({ownerId: userDoc._id}).then(cars => {
+      carModel.find({ ownerId: userDoc._id }).then(cars => {
         console.log(cars);
-        res.render("user_page.hbs", { userDoc, cars });
+        pieceModel.find({ ownerId: userDoc._id }).then(pieces => {
+          console.log(pieces);
+          res.render("user_page.hbs", { userDoc, cars, pieces });
+        });
       });
     })
     .catch(err => next(err));
 });
-module.exports = router;
+
 //--------------Get car data----------
 router.get("/dashboard_car", (req, res, next) => {
   carModel
--- routes/api_piece.js
+++ routes/api_piece.js
@@ -3,40 +3,45 @@ const router = new express.Router();
 const pieceModel = require("../models/piece");
 
 // ------------------------------------------------------
-// creat car 
+// creat car
 // -------------------------------------------------------
 
-router.get("/form_piece",(req,res,next)=>{
-  res.render("form_piece.hbs")
+router.get("/form_piece", (req, res, next) => {
+  res.render("form_piece.hbs");
 });
 
-router.post("/piece",(req,res,next)=>{
-  // const {carModel, model,color,year,descriptif,imag}= req.body 
-  pieceModel.create(req.body)
-  .then (carData =>{
-    res.redirect("/")
-    console.log(carData)
-  })
-  .catch(err =>next(err))
+router.post("/piece", (req, res, next) => {
+  // const {pieceModel, model,color,year,descriptif,imag}= req.body
+  pieceModel
+    .create(req.body)
+    .then(pieceData => {
+      res.locals.pieceData = pieceData;
+      res.redirect("/user_page/" + pieceData.ownerId);
+      console.log("Piece ADDED");
+    })
+    .catch(err => next(err));
 });
 //--------------Get user Details----------
-router.get("/user_page/:id",(req,res,next)=>{
-  console.log(req.params.id)
-  userModel.findById(req.params.id)
-  .then (userDoc =>{
-    console.log(userDoc);
-    res.render("user_page.hbs",{ userDoc })
-  })
-  .catch(err => next(err))
+
+//--------------Get piece data----------
+router.get("/dashboard_piece", (req, res, next) => {
+  pieceModel
+    .find()
+    .then(pieceData => {
+      console.log(pieceData);
+      res.render("dashboard_piece.hbs", { pieceData });
+    })
+    .catch(err => next(err));
 });
-//--------------Get Piece data----------
-router.get("/dashboard_piece",(req,res,next)=>{
-  pieceModel.find()
-  .then (pieceData =>{
-    console.log(pieceData);
-    res.render("dashboard_piece.hbs",{ pieceData })
-  })
-  .catch(err => next(err))
+
+router.get("/", (req, res, next) => {
+  pieceModel
+    .find()
+    .then(pieceData => {
+      console.log(pieceData);
+      res.render("index.hbs", { pieceData });
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
